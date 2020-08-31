e9ca0064404f35f9ca025a2c6726cc833e44e94e
Duduc314 <nicolasmoges@gmail.com>
2020-08-31 21:27:14 +0200

    Initial commit

---
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -0,0 +1,36 @@
+package com.openclassrooms.magicgithub.repository;
+
+import com.openclassrooms.magicgithub.api.ApiService;
+import com.openclassrooms.magicgithub.model.User;
+
+import java.util.List;
+
+public class UserRepository {
+
+    private final ApiService apiService; // TODO: A utiliser
+
+    public UserRepository(ApiService apiService) {
+        this.apiService = apiService;
+    }
+
+    public List<User> getUsers() {
+        // TODO: A modifier
+        // --------- UPDATE CODE -------------------------
+        return apiService.getUsers(); // return null
+        // -----------------------------------------------
+    }
+
+    public void generateRandomUser() {
+        // TODO: A modifier
+        // --------- UPDATE CODE -------------------------
+        apiService.generateRandomUser();
+        // -----------------------------------------------
+    }
+
+    public void deleteUser(User user) {
+        // TODO: A modifier
+        // --------- UPDATE CODE -------------------------
+        apiService.deleteUser(user);
+        // -----------------------------------------------
+    }
+}
